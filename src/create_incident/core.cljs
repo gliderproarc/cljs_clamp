f044bf40d6bc3e460f5b0237664d96f095c9baa1
Robert Clay <gliderproarc@gmail.com>
2022-10-17 14:05:21 +0900

    first commit of new fork

--- src/create_incident/core.cljs
+++ src/create_incident/core.cljs
@@ -1,8 +1,11 @@
+; https://lambdaisland.com/blog/2016-10-01-clojurescript-and-google-apps-script
 (ns create-incident.core
   (:require [create-incident.entrypoints]))
 
 (defn ^:export hello-world []
   (.. js/SpreadsheetApp
-    getActiveSheet
-    (appendRow #js ["hello", "world"])))
-; https://lambdaisland.com/blog/2016-10-01-clojurescript-and-google-apps-script
+      getActiveSheet
+      (appendRow #js ["hello", "world"])))
+
+(defn ^:export double-me [num]
+  (* num 2))
--- src/entrypoints.js
+++ src/entrypoints.js
@@ -1,4 +1,17 @@
-
+// vestigle hello world.
 function main() {
   create_incident.core.hello_world();
+}
+
+// Below is a template that can use used for new GoogleAppsScript Sheets functions
+
+/**
+ * This text shows the description of the sheet function
+ *
+ * @param {number} input The type and name of the functions first input
+ * @return {number} the type of what comes out
+ * @customfunction
+ */
+function doubleMe(input) {
+    return create_incident.core.double_me(num);
 }
